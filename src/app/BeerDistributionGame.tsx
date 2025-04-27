dba4c466bdc1dfb0255db2f9d194b50711053409
Bian Lee <bianlee2004@gmail.com>
2025-04-27 04:22:13 -0700

    .

--- src/app/BeerDistributionGame.tsx
+++ src/app/BeerDistributionGame.tsx
@@ -612,15 +612,6 @@ const BeerDistributionGame = () => {
             Show Instructions
           </button>
         )}
-        
-        {!gameStarted && showInstructions && (
-          <button 
-            className="mt-6 px-6 py-2 bg-green-600 text-white rounded hover:bg-green-700 w-full"
-            onClick={startNewGame}
-          >
-            Start Game
-          </button>
-        )}
       </div>
       
       {/* CSS for animations */}
